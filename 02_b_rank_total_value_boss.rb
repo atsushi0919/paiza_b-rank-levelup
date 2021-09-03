50d58b4054d0be9a65e116357a0b7b4b6d6513c4
atsushi0919 <hatake0919@yahoo.co.jp>
2021-09-03 16:39:47 +0900

    init

---
+++ 02_b_rank_total_value_boss.rb
@@ -0,0 +1,132 @@
+# 足すか掛けるか (paizaランク C 相当)
+# https://paiza.jp/works/mondai/prob60/total_value_9
+
+INPUT1 = <<~"EOS"
+  5
+  2 1
+  3 6
+  4 2
+  4 4
+  4 70
+EOS
+OUTPUT1 = <<~"EOS"
+  108
+EOS
+
+INPUT2 = <<~"EOS"
+  1
+  0 0
+EOS
+OUTPUT2 = <<~"EOS"
+  0
+EOS
+
+INPUT3 = <<~"EOS"
+  3
+  1 2
+  3 4
+  5 6
+EOS
+OUTPUT3 = <<~"EOS"
+  21
+EOS
+
+=begin
+def solve(input_lines)
+  input_lines = input_lines.split("\n")
+  n = input_lines.shift.to_i
+  ary = []
+  input_lines.shift(n).each do |line|
+    ary.push(line.split.map(&:to_i))
+  end
+
+  result = 0
+  ary.each do |a, b|
+    if a == b
+      result += a * b
+    else
+      result += a + b
+    end
+  end
+  result
+end
+=end
+
+def solve(input_lines)
+  _, *ary = input_lines.split("\n")
+
+  result = ary.map { |item|
+    a, b = item.split.map(&:to_i)
+    a == b ? a * b : a + b
+  }.sum
+end
+
+p solve(INPUT1)
+
+=begin
+足すか掛けるか (paizaランク C 相当)
+問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
+
+シェア用URL:
+https://paiza.jp/works/mondai/prob60/total_value_9
+問題文のURLをコピーする
+Img 04 03 下記の問題をプログラミングしてみよう！
+2つの整数の組がn個与えられるので、各組の計算結果を足し合わせたものを出力してください。
+各組の計算結果は次の値です。
+・2つの整数の組を足し合わせたもの
+・ただし、2つの整数が同じ値だった場合は、掛け合わせたもの
+
+▼　下記解答欄にコードを記入してみよう
+
+入力される値
+入力は以下のフォーマットで与えられます。
+
+n
+a_1 b_1
+...
+a_n b_n
+
+nは与えられる整数の組の行数です。
+a_iとb_iはそれぞれが整数です。
+
+
+入力値最終行の末尾に改行が１つ入ります。
+文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
+期待する出力
+各行の計算結果を足し合わせた値を出力してください。
+最後は改行し、余計な文字、空行を含んではいけません。
+
+条件
+すべてのテストケースにおいて、以下の条件を満たします。
+
+1 ≦ n ≦ 50
+-100,000 ≦ a_n ≦ 100,000
+-100,000 ≦ b_n ≦ 100,000
+
+入力例1
+5
+2 1
+3 6
+4 2
+4 4
+4 70
+
+出力例1
+108
+
+入力例2
+1
+0 0
+
+出力例2
+0
+
+入力例3
+3
+1 2
+3 4
+5 6
+
+出力例3
+21
+=end
---
+++ README.md
@@ -0,0 +1,3 @@
+# paiza_b-rank-levelup
+
+2021/9/2 ~
